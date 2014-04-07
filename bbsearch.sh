d6ceb72c45339e9df7a814b919c02189203fb922
Naveen Malik <nmalik@redhat.com>
2014-04-07 17:08:59 -0400

    Added initial search script

---
+++ bbsearch.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+IFS=$'\n'
+
+echo "Searching for '$@'..."
+GREP_STRING="grep --with-filename --label=\"\1\""
+for X in $@
+do
+    GREP_STRING="$GREP_STRING -e \"$X\""
+done;
+#echo $GREP_STRING
+find $HOME/Documents/bbsync/ -maxdepth 1 -name '*.pdf' | sed "s#\(.*\)#pdftotext \1 - | $GREP_STRING#g" | sh | sort | uniq
+
